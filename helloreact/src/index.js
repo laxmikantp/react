70c40c3b054ee2214cc1e7293448dc902792a31f
laxmikant <laxmikant@holidayiq.com>
2017-05-03 13:36:41 +0530

    react project

---
+++ helloreact/src/index.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import './index.css';
+import TodoItem from './component/TodoItem.js';
+import TodoForm from './component/TodoForm.js';
+
+class TodoList extends React.Component {
+    constructor(){
+    super();
+    this.changeStatus = this.changeStatus.bind(this);
+    this.updateTask = this.updateTask.bind(this);
+    this.addTask = this.addTask.bind(this);
+    this.deleteTask = this.deleteTask.bind(this);
+    this.editItem=this.editItem.bind(this);
+    this.state = {
+        tasks:[{
+            name:"Buy Milk",
+            completed:false
+        },
+            {
+                name:"Buy Cheese",
+                completed:false
+            },
+            {
+                name:"Buy Bread",
+                completed:false
+            }],
+        currentTask:''
+    }
+}
+
+    updateTask(newValue){
+    this.setState({
+        currentTask:newValue.target.value
+      })
+    }
+
+
+    editItem(index,newValue){
+
+    var tasks = this.state.tasks;
+    var task = tasks[index];
+    task['name']=newValue;
+    this.setState({
+        tasks
+    })
+    }
+
+    deleteTask(index){
+    let tasks=this.state.tasks;
+    tasks.splice(index,1);
+    this.setState({
+        tasks
+    })
+}
+
+    addTask(evt){
+    evt.preventDefault();
+    var tasks=this.state.tasks;
+    var currentTask=this.state.currentTask;
+
+    tasks.push({
+    name:currentTask,
+        completed:false
+    })
+
+    this.setState({
+        tasks,
+        currentTask:''
+    })
+
+    }
+    changeStatus(index){
+    var tasks = this.state.tasks;
+    var task = tasks[index];
+    task.completed = !task.completed;
+    this.setState({
+        tasks:tasks
+    })
+}
+    render() {
+    return (
+        <section>
+            <TodoForm currentTask={this.state.currentTask} updateTask={this.updateTask} addTask={this.addTask} />
+        <ul>
+      {
+          this.state.tasks.map((task, index) => {
+              return <TodoItem key={index} clickHandler={this.changeStatus} index={index} details={task} editItem={this.editItem} deleteTask={this.deleteTask} />
+        })
+        }
+
+        </ul>
+        </section>
+        )
+}
+}
+
+
+
+ReactDOM.render(<TodoList />,document.getElementById('root'))
+
